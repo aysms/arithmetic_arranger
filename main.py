12d38e86e070e4532aa6e2a860551b7478d35528
aysms <85144734+aysms@users.noreply.github.com>
2021-08-08 11:27:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from arithmetic_arranger import arithmetic_arranger
 from unittest import main
 
 
-#print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
+print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
 
-#print(arithmetic_arranger(["3213 + 698", "1234 + 356"], True))
+
 # Run unit tests automatically
 main(module='test_module', exit=False)
